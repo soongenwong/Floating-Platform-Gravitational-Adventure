1ad4475583dc042088ca16a463d88cdfa70b476f
Max Ryan <161385834+maxryan4@users.noreply.github.com>
2025-03-02 19:29:02 +0000

    Platforms spawn at set intervals

--- game/scripts/platform.gd
+++ game/scripts/platform.gd
@@ -4,10 +4,10 @@ extends Node2D
 @export var platform_break_scene: PackedScene
 @export var platform_moving_scene: PackedScene
 @export var coin_scene: PackedScene
-@export var spawn_count: int = 200
+@export var spawn_count: int = 100
 @export var spawn_break_count: int = 25
 @export var spawn_moving_count: int = 25
-@export var spawn_range_x: Vector2 = Vector2(-150, 150)
+@export var spawn_range_x: Vector2 = Vector2(-100, 100)
 @export var spawn_range_y: Vector2 = Vector2(-1500, 0)
 
 func _ready():
@@ -38,10 +38,7 @@ func _ready():
 func spawn_platforms():
 	for i in range(spawn_count):
 		var platform = platform_scene.instantiate()
-		platform.position = Vector2(
-			randf_range(spawn_range_x.x, spawn_range_x.y),
-			randf_range(spawn_range_y.x, spawn_range_y.y)
-		)
+		platform.position = get_pos(i, spawn_count)
 		GameManager.platform_pos.append(platform.position)
 		
 		add_child(platform)
@@ -49,20 +46,14 @@ func spawn_platforms():
 func spawn_break():
 	for i in range(spawn_break_count):
 		var platform_break = platform_break_scene.instantiate()
-		platform_break.position = Vector2(
-			randf_range(spawn_range_x.x, spawn_range_x.y),
-			randf_range(spawn_range_y.x, spawn_range_y.y)
-		)
+		platform_break.position = get_pos(i, spawn_break_count)
 		GameManager.platform_break_pos.append(platform_break.position)
 		add_child(platform_break)
 		
 func spawn_moving():
 	for i in range(spawn_moving_count):
 		var platform_moving = platform_moving_scene.instantiate()
-		platform_moving.position = Vector2(
-			randf_range(spawn_range_x.x, spawn_range_x.y),
-			randf_range(spawn_range_y.x, spawn_range_y.y)
-		)
+		platform_moving.position = get_pos(i, spawn_moving_count)
 		GameManager.platform_moving_pos.append(platform_moving.position)
 		add_child(platform_moving)
 
@@ -70,3 +61,6 @@ func spawn_coin():
 	var coin = coin_scene.instantiate()
 	coin.position = Vector2(0, spawn_range_y.x)
 	add_child(coin)
+	
+func get_pos(num, count):
+	return Vector2(randf_range(spawn_range_x.x, spawn_range_x.y), spawn_range_y.x/count * num)
