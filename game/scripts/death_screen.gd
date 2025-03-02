7af8a65938f2f4a6a48df8775199e9d82d809268
Max Ryan <161385834+maxryan4@users.noreply.github.com>
2025-03-02 19:41:47 +0000

    Fixed bug where on 2nd game score wouldn't increase

--- game/scripts/death_screen.gd
+++ game/scripts/death_screen.gd
@@ -11,5 +11,5 @@ func _on_restart_button_pressed() -> void:
 
 func start_game():
 	#print("going to start screen")
+	GameManager.score = 0
 	get_tree().change_scene_to_file("res://scenes/start_screen.tscn")
-	pass
