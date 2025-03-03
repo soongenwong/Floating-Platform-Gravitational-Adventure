c9522c03accd8eea181581ac9f96848ec38d0067
Soong En Wong <wsoongen2015@gmail.com>
2025-03-03 12:22:46 +0000

    implemented server connection using tcp

--- AWS/game_server/server.js
+++ AWS/game_server/server.js
@@ -1,49 +1,153 @@
-const WebSocket = require('ws');
-
-const wss = new WebSocket.Server({ port: 8080 });
+const net = require('net');
 
 // Store connected clients
-const clients = new Set();
+const clients = new Map(); 
 // Store current game state
-let gameState = {};
+let gameState = {
+    players: {},
+    gameInProgress: false
+};
 
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  clients.add(ws);
-  
-  // Send current game state to new client
-  ws.send(JSON.stringify({
-    type: 'state_update',
-    data: gameState
-  }));
-  
-  ws.on('message', (message) => {
-    try {
-      const data = JSON.parse(message);
-      console.log('Received:', data);
-      
-      if (data.type === 'state_update') {
-        gameState = {...gameState, ...data.data};
+const server = net.createServer((socket) => {
+    console.log('Client connected');
+    const clientId = generateClientId();
+    clients.set(socket, clientId);
+    
+    let buffer = '';
+    
+    socket.on('data', (data) => {
+        buffer += data.toString();
         
-        // Broadcast to all clients
-        clients.forEach((client) => {
-          if (client !== ws && client.readyState === WebSocket.OPEN) {
-            client.send(JSON.stringify({
-              type: 'state_update',
-              data: gameState
-            }));
-          }
+        const messages = buffer.split('\n');
+        buffer = messages.pop();
+        
+        messages.forEach(message => {
+            if (message.length > 0) {
+                try {
+                    const parsedData = JSON.parse(message);
+                    console.log('Received:', parsedData);
+                    
+                    handleMessage(socket, parsedData);
+                } catch (e) {
+                    console.error('Error parsing message:', e);
+                }
+            }
         });
-      }
-    } catch (e) {
-      console.error('Error parsing message:', e);
-    }
-  });
-  
-  ws.on('close', () => {
-    console.log('Client disconnected');
-    clients.delete(ws);
-  });
+    });
+    
+    socket.on('close', () => {
+        console.log('Client disconnected:', clients.get(socket));
+        // Handle player disconnection
+        const playerId = clients.get(socket);
+        if (gameState.players[playerId]) {
+            delete gameState.players[playerId];
+            // Notify other players someone left
+            broadcastGameState();
+        }
+        clients.delete(socket);
+    });
+    
+    socket.on('error', (err) => {
+        console.error('Socket error:', err);
+        clients.delete(socket);
+    });
 });
 
-console.log('Game server running on port 8080');
+function handleMessage(socket, message) {
+    const playerId = clients.get(socket);
+    
+    switch (message.type) {
+        case 'player_register':
+            registerPlayer(socket, message.player_id);
+            break;
+            
+        case 'player_action':
+            processPlayerAction(message.player_id, message.action);
+            break;
+            
+        case 'state_update':
+            
+            break;
+    }
+}
+
+function registerPlayer(socket, requestedId) {
+    const playerId = requestedId || generateClientId();
+    clients.set(socket, playerId);
+    
+    // Add player to game state
+    gameState.players[playerId] = {
+        id: playerId,
+        x: Math.floor(Math.random() * 300) + 100, 
+        y: Math.floor(Math.random() * 200) + 100,
+        score: 0
+    };
+    
+    // Confirm player registration
+    socket.write(JSON.stringify({
+        type: 'player_assigned',
+        player_id: playerId
+    }) + '\n');
+    
+    console.log(`Player registered: ${playerId}`);
+    console.log(`Total players: ${Object.keys(gameState.players).length}`);
+    
+    if (Object.keys(gameState.players).length >= 2 && !gameState.gameInProgress) {
+        startGame();
+    }
+    
+    broadcastGameState();
+}
+
+function processPlayerAction(playerId, action) {
+    // Update game state based on player action
+    if (gameState.players[playerId]) {
+        switch(action.type) {
+            case 'move':
+                gameState.players[playerId].x = action.x;
+                gameState.players[playerId].y = action.y;
+                break;
+            case 'attack':
+                // Handle attack logic
+                break;
+            // Add other action types as needed
+        }
+        
+        broadcastGameState();
+    }
+}
+
+function startGame() {
+    gameState.gameInProgress = true;
+    const playerIds = Object.keys(gameState.players);
+    
+    for (const [socket, id] of clients.entries()) {
+        socket.write(JSON.stringify({
+            type: 'game_start',
+            players: playerIds
+        }) + '\n');
+    }
+    
+    console.log('Game started with players:', playerIds);
+}
+
+function broadcastGameState() {
+    const gameStateMsg = JSON.stringify({
+        type: 'state_update',
+        data: gameState
+    }) + '\n';
+    
+    for (const [socket, id] of clients.entries()) {
+        if (!socket.destroyed) {
+            socket.write(gameStateMsg);
+        }
+    }
+}
+
+function generateClientId() {
+    return Math.random().toString(36).substring(2, 15);
+}
+
+server.listen(8080, () => {
+    console.log('Game server running on port 8080');
+});
--- AWS/game_server/server_connection.gd
+++ AWS/game_server/server_connection.gd
@@ -1,50 +1,91 @@
 extends Node
 
-   var socket = WebSocketPeer.new()
-   var server_url = "ws://18.170.74.19:8080"  
-   var _ws_url = "ws://localhost:8080"
-   var game_state = {}
-   var connected = false
+var socket = StreamPeerTCP.new()
+var server_ip = "127.0.0.1"
+var server_port = 8080
+var local_ip = "127.0.0.1"
+var connected = false
+var game_state = {}
+var read_buffer = ""
+var player_id = ""  
 
-   func _ready():
-       var err = socket.connect_to_url(server_url)
-       if err != OK:
-           print("Unable to connect to server")
+func _ready():
+    player_id = str(randi())
+    connect_to_server()
 
-   func _process(delta):
-       socket.poll()
-       var state = socket.get_ready_state()
-       
-       if state == WebSocketPeer.STATE_OPEN:
-           if !connected:
-               connected = true
-               print("Connected to server!")
-           
-           # Check for received data
-           while socket.get_available_packet_count() > 0:
-               var data = socket.get_packet().get_string_from_utf8()
-               var json = JSON.parse_string(data)
-               if json and json.has("type") and json.type == "state_update":
-                   game_state = json.data
-                   update_game_with_state(game_state)
-                   
-       elif state == WebSocketPeer.STATE_CLOSING:
-           connected = false
-           print("Connection is closing...")
-       elif state == WebSocketPeer.STATE_CLOSED:
-           connected = false
-           var code = socket.get_close_code()
-           var reason = socket.get_close_reason()
-           print("Connection closed with code: %d, reason: %s" % [code, reason])
+func connect_to_server():
+    print("Attempting to connect to " + server_ip + ":" + str(server_port))
+    var err = socket.connect_to_host(server_ip, server_port)
+    if err != OK:
+        print("Unable to connect to server: ", err)
+    else:
+        print("Connection initiated...")
 
-   func send_game_state(state_data):
-       if connected:
-           var data = {
-               "type": "state_update",
-               "data": state_data
-           }
-           var json_str = JSON.stringify(data)
-           socket.send_text(json_str)
-           
-   func update_game_with_state(state):
-       pass
+func _process(delta):
+    if socket.get_status() == StreamPeerTCP.STATUS_CONNECTING:
+        print("Connecting...")
+    elif socket.get_status() == StreamPeerTCP.STATUS_CONNECTED:
+        if !connected:
+            connected = true
+            print("Connected to server!")
+            register_player()
+            
+        if socket.get_available_bytes() > 0:
+            var data = socket.get_utf8_string(socket.get_available_bytes())
+            read_buffer += data
+            
+            var messages = read_buffer.split("\n")
+            if messages.size() > 1:
+                read_buffer = messages[messages.size() - 1]
+                
+                for i in range(messages.size() - 1):
+                    var message = messages[i]
+                    if message.length() > 0:
+                        process_message(message)
+    elif socket.get_status() == StreamPeerTCP.STATUS_NONE or socket.get_status() == StreamPeerTCP.STATUS_ERROR:
+        if connected:
+            connected = false
+            print("Disconnected from server")
+
+func register_player():
+    if connected:
+        var data = {
+            "type": "player_register",
+            "player_id": player_id
+        }
+        var json_str = JSON.stringify(data) + "\n"
+        socket.put_data(json_str.to_utf8_buffer())
+
+func process_message(message):
+    var json = JSON.parse_string(message)
+    if json == null:
+        print("Failed to parse message: ", message)
+        return
+        
+    if json.has("type"):
+        match json.type:
+            "state_update":
+                game_state = json.data
+                update_game_with_state(game_state)
+            "player_assigned":
+                player_id = json.player_id
+                print("Assigned player ID: ", player_id)
+            "game_start":
+                print("Game started with players: ", json.players)
+                # Update UI to show both players are connected
+            "game_end":
+                print("Game ended. Winner: ", json.winner)
+
+func send_game_action(action_data):
+    if connected:
+        var data = {
+            "type": "player_action",
+            "player_id": player_id,
+            "action": action_data
+        }
+        var json_str = JSON.stringify(data) + "\n"
+        socket.put_data(json_str.to_utf8_buffer())
+        
+func update_game_with_state(state):
+    
+    pass
