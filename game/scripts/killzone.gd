0331b940f85fd287068ff7612fcb8bb8593cc4f1
Max Ryan <161385834+maxryan4@users.noreply.github.com>
2025-03-05 18:18:16 +0000

    Minor game changes

--- game/scripts/killzone.gd
+++ game/scripts/killzone.gd
@@ -5,7 +5,7 @@ extends Area2D
 
 func _process(delta: float) -> void:
 	position.y -= speed * delta
-	speed = delta/6 + speed
+	speed = delta/4 + speed
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
--- game/scripts/platform.gd
+++ game/scripts/platform.gd
@@ -4,7 +4,7 @@ extends Node2D
 @export var platform_break_scene: PackedScene
 @export var platform_moving_scene: PackedScene
 @export var coin_scene: PackedScene
-@export var spawn_count: int = 300
+@export var spawn_count: int = 290
 @export var spawn_break_count: int = 160
 @export var spawn_moving_count: int = 120
 @export var spawn_range_x: Vector2 = Vector2(-150, 150)
@@ -28,32 +28,26 @@ func _ready():
 			var platform_moving = platform_moving_scene.instantiate()
 			platform_moving.position = pos
 			add_child(platform_moving)
-		
-		
 	spawn_coin()
-	
-	# share platform_locations to aws
-	
 
 func spawn_platforms():
 	for i in range(spawn_count):
 		var platform = platform_scene.instantiate()
 		platform.position = get_pos(i, spawn_count)
 		GameManager.platform_pos.append(platform.position)
-		
 		add_child(platform)
 
 func spawn_break():
 	for i in range(spawn_break_count):
 		var platform_break = platform_break_scene.instantiate()
-		platform_break.position = get_pos(i, spawn_break_count)
+		platform_break.position = get_pos_special(i, spawn_break_count)
 		GameManager.platform_break_pos.append(platform_break.position)
 		add_child(platform_break)
 		
 func spawn_moving():
 	for i in range(spawn_moving_count):
 		var platform_moving = platform_moving_scene.instantiate()
-		platform_moving.position = get_pos(i, spawn_moving_count)
+		platform_moving.position = get_pos_special(i, spawn_moving_count)
 		GameManager.platform_moving_pos.append(platform_moving.position)
 		add_child(platform_moving)
 
@@ -64,3 +58,6 @@ func spawn_coin():
 	
 func get_pos(num, count):
 	return Vector2(randf_range(spawn_range_x.x, spawn_range_x.y), spawn_range_y.x/count * num)
+
+func get_pos_special(num, count):
+	return Vector2(randf_range(spawn_range_x.x, spawn_range_x.y), (spawn_range_y.x+200)/count * num -200)
