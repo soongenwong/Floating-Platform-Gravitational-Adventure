0c673d22651037d2d4bb7638241fe8faf4c91649
Bhargav-Shivakumar <bhargavshiv23@gmail.com>
2025-03-10 17:48:33 +0000

    TWO PLAYER WORKKSS!!!!!

--- game/scripts/player_2.gd
+++ game/scripts/player_2.gd
@@ -1,7 +1,4 @@
 extends CharacterBody2D
 
 func _process(delta: float) -> void:
-	#print(GameManager.other_player_pos)
 	position = GameManager.other_player_pos
-	#position = GameManager.other_player_pos
-	#print("player2physics: ", position)
